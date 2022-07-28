15d3abff6b2b3aaf910f63a381430c2feacf0dfd
nazhudha <57568434+nazhudha@users.noreply.github.com>
2022-07-28 17:57:27 +0100

    Account class with basic functions

--- account.js
+++ account.js
@@ -1,10 +1,25 @@
 class Account {
-  // constructor(balance) {
-  //   this.transactions = balance;
-  // }
-  balance(){
-    return "works"
+  constructor() {
+    this.balance = 0;
+    this.transactions = [];
   }
+
+  deposit(amount) {
+    this.balance -= amount
+  }
+
+  withdraw(amount) {
+    this.balance += amount
+  }
+
+  showTranscations() {
+    return this.transactions;
+  }
+
+  showBalance() {
+    return this.balance;
+  }
+
 }
 
 module.exports = Account;
