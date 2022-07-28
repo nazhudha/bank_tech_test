976842e67bccf17e9484f84cd5ed817be1c725c1
nazhudha <57568434+nazhudha@users.noreply.github.com>
2022-07-28 18:11:04 +0100

    add and withdraw some funds, working

--- test/account.test.js
+++ test/account.test.js
@@ -1,9 +1,15 @@
-const Account = require('../account')
+const Account = require("../account");
 
 let account = new Account();
 
-describe('Account Class', () => {
-  it('creates new empty bank Account with 0 balance', () => {
-    expect(account.showBalance()).toBe(0)
-  })
-})
+describe("Account Class", () => {
+  it("creates new empty bank Account with 0 balance", () => {
+    expect(account.showBalance()).toBe(0);
+  });
+
+  it("Desposit 500, then withdraw 100, and show balance 400", () => {
+    account.deposit(500);
+    account.withdraw(100);
+    expect(account.showBalance()).toBe(400);
+  });
+});
